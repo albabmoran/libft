da68eb6985e7abf7c6ba40bdbb078dc03bcba7b3
Alba Ben Amed Moran <albben-a@student.42madrid.com>
2026-01-21 13:06:55 +0100

    split and strtrim

---
+++ ft_split.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albben-a <albben-a@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/21 10:49:21 by albben-a          #+#    #+#             */
+/*   Updated: 2026/01/21 11:49:42 by albben-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	count_words(const char *s, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c && (i == 0 || s[i - 1] == c))
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+static int	count_char(const char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	return (i);
+}
+
+static void	*free_all(char **array, int i)
+{
+	while (i >= 0)
+	{
+		if (array[i])
+			free(array[i]);
+		i--;
+	}
+	free(array);
+	return (NULL);
+}
+
+static char	**create_array(char const *s, char c, char **array)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	k = 0;
+	while (i < count_words(s, c))
+	{
+		j = 0;
+		while (s[k] == c)
+			k++;
+		array[i] = malloc(count_char(&s[k], c) + 1);
+		if (!array[i])
+			return (free_all(array, i));
+		while (s[k] && s[k] != c)
+		{
+			array[i][j] = s[k];
+			k++;
+			j++;
+		}
+		array[i][j] = '\0';
+		i++;
+	}
+	return (array);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**array;
+
+	array = malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (!array)
+		return (NULL);
+	create_array(s, c, array);
+	array[count_words(s, c)] = NULL;
+	return (array);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	**s;
+	char	str[] = ",,,,,hola, que,tal";
+	char	c = ',';
+	int		i;
+
+	s = ft_split(str, c);
+	i = 0;
+	while (s[i])
+	{
+		printf("%s\n", s[i]);
+		i++;
+	}
+	free(s);
+	return (0);
+}*/
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,17 +6,85 @@
 /*   By: albben-a <albben-a@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 17:01:48 by albben-a          #+#    #+#             */
-/*   Updated: 2026/01/20 17:22:05 by albben-a         ###   ########.fr       */
+/*   Updated: 2026/01/21 13:06:12 by albben-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	find_start(const char *s1, const char *set)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (set[j])
+	{
+		if (s1[i] && s1[i] == set[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			j++;
+	}
+	return (i);
+}
+
+static int	find_end(const char *s1, const char *set, int i)
+{
+	int	len;
+	int	j;
+
+	len = ft_strlen(s1);
+	j = 0;
+	while (set[j])
+	{
+		if (s1[len - 1] && len - 1 > i && s1[len - 1] == set[j])
+		{
+			len--;
+			j = 0;
+		}
+		else
+			j++;
+	}
+	return (len);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*str;
+	int		i;
+	int		j;
+	int		len;
 
-	str = malloc(ft_strlen(s1) - ft_strlen(set) + 1);
+	i = find_start(s1, set);
+	len = find_end(s1, set, i);
+	str = malloc(len - i + 1);
 	if (!str)
 		return (NULL);
+	j = 0;
+	while (i < len)
+	{
+		str[j] = s1[i];
+		i++;
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
 }
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	s1[] = "abchhohohoadioshohhoo";
+	char	set[] = "abcho";
+	char	*str;
+
+	str = ft_strtrim(s1, set);
+	printf("%s", str);
+	free(str);
+	return (0);
+}*/
